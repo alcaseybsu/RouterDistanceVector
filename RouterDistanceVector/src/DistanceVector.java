9d16955f386df2b0e0abeb13716991489f321bc7
An-Unavailable-Username <111703932+An-Unavailable-Username@users.noreply.github.com>
2024-02-29 16:11:47 -0500

    Add files via upload
    Signed-off-by: An-Unavailable-Username <111703932+An-Unavailable-Username@users.noreply.github.com>

---
+++ RouterDistanceVector/src/DistanceVector.java
@@ -0,0 +1,6 @@
+//enables the router to calculate the
+// distance between itself and every subnet
+
+public class DistanceVector {
+}
+// git push origin mason
---
+++ RouterDistanceVector/src/Router.java
@@ -0,0 +1,6 @@
+public class Router {
+    
+}
+// Grade is on:
+//Ability to print out its distance vector to each subnet
+//  AND the next hop from that router to each subnet
